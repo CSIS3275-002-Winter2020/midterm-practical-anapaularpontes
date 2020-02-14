09a401fbb4eacbf1c2dfca736a18ec4ee9385242
Ruiz Pontes <ruizpontesa@student.douglascollege.ca>
2020-02-14 12:57:43 -0800

    Added class to print name and email

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ana Paula Ruiz Pontes");
+		System.out.println("ruizpontesa@student.douglascollege.ca");
+	}
+
+}
